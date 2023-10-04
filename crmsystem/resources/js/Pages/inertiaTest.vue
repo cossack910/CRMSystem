b097f07b6769394f12841309d9ba3a9b3c3558a3
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-10-04 21:13:59 +0900

    test

---
+++ crmsystem/resources/js/Pages/inertiaTest.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    testets<br />
+    <a href="/">a tag</a> <br />
+    <Link href="/">Link経由です</Link>
+</template>
