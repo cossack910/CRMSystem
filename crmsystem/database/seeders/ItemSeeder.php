d867fd8db0d6f79e993f6f91e2f6a72fe0887408
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-11-17 08:34:36 +0900

    ナビメニュー

--- crmsystem/database/seeders/ItemSeeder.php
+++ crmsystem/database/seeders/ItemSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class ItemSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カット詳細',
+                'price' => 6000,
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラー詳細',
+                'price' => 8000,
+            ],
+            [
+                'name' => 'パーマ',
+                'memo' => 'パーマ詳細',
+                'price' => 13000,
+            ]
+        ]);
     }
 }
