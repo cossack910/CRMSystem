cc3d33c1a95181e87543f3ed2bd032984ed366be
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-12-08 09:31:54 +0900

    end

--- crmsystem/app/Http/Controllers/PurchaseController.php
+++ crmsystem/app/Http/Controllers/PurchaseController.php
@@ -8,6 +8,7 @@ use App\Models\Customer;
 use App\Models\Item;
 use App\Models\Purchase;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\{DB, Log};
 
 class PurchaseController extends Controller
 {
@@ -38,7 +39,27 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        dd($request);
+        DB::beginTransaction();
+        try {
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status,
+            ]);
+    
+            foreach($request->items as $item) {
+                $purchase->items()->attach(
+                    $purchase->id,
+                    [
+                        'item_id' => $item['id'],
+                        'quantity' => $item['quantity']
+                     ]);
+            }
+            DB::commit();
+            return to_route('dashboard');
+        } catch(\Exception $e) {
+            Log::alert("購入登録処理失敗");
+            DB::rollBack();
+        }
     }
 
     /**
