3b6ed92d7e053b4c443df3fad68b24948d645905
cossack910 <maris160zhk@gmail.com>
2023-12-28 16:37:51 +0900

    end

--- crmsystem/resources/js/Pages/Purchases/Show.vue
+++ crmsystem/resources/js/Pages/Purchases/Show.vue
@@ -2,71 +2,27 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, router } from "@inertiajs/vue3";
 import { onMounted, reactive, ref, computed } from "vue";
-import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
-import { getToday } from "@/common";
-import MicroModal from "@/Components/MicroMordal.vue";
+import dayjs from "dayjs";
 
 const props = defineProps({
     items: Array,
     order: Array,
 });
 
-const itemList = ref([]);
-
 onMounted(() => {
     console.log(props.items);
     console.log(props.order[0].customer_name);
-    form.date = getToday();
-    props.items.forEach((item) => {
-        itemList.value.push({
-            id: item.id,
-            name: item.name,
-            price: item.price,
-            quantity: 0,
-        });
-    });
 });
-
-const form = reactive({
-    date: null,
-    customer_id: null,
-    status: true,
-    items: [],
-});
-
-const totalPrice = computed(() => {
-    let total = 0;
-    itemList.value.forEach((item) => {
-        total += item.price * item.quantity;
-    });
-    return total;
-});
-
-const storePurchase = () => {
-    itemList.value.forEach((item) => {
-        if (item.quantity > 0) {
-            form.items.push({
-                id: item.id,
-                quantity: item.quantity,
-            });
-        }
-    });
-    router.post(route("purchases.store"), form);
-};
-
-const setCustomerId = (id) => {
-    form.customer_id = id;
-};
 
 const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 </script>
 
 <template>
-    <Head title="購入画面" />
+    <Head title="購買履歴 詳細画面" />
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                購入画面
+                購買履歴 詳細画面
             </h2>
         </template>
 
@@ -75,7 +31,6 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <section class="text-gray-600 body-font relative">
-                            <!--<BreezeValidationErrors :errors="errors" />-->
                             <form @submit.prevent="storePurchase">
                                 <div class="container px-5 py-8 mx-auto">
                                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
@@ -87,13 +42,21 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                                                         class="leading-7 text-sm text-gray-600"
                                                         >日付</label
                                                     >
-                                                    <input
+                                                    <div
                                                         type="date"
                                                         id="date"
                                                         name="date"
-                                                        v-model="form.date"
                                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    />
+                                                    >
+                                                        {{
+                                                            dayjs(
+                                                                props.order[0]
+                                                                    .created_at
+                                                            ).format(
+                                                                "YYYY/MM/DD"
+                                                            )
+                                                        }}
+                                                    </div>
                                                 </div>
 
                                                 <div class="relative">
@@ -102,11 +65,16 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                                                         class="leading-7 text-sm text-gray-600"
                                                         >会員名</label
                                                     >
-                                                    <MicroModal
-                                                        @update:customer-id="
-                                                            setCustomerId
-                                                        "
-                                                    />
+                                                    <div
+                                                        id="customer_name"
+                                                        name="customer_name"
+                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    >
+                                                        {{
+                                                            props.order[0]
+                                                                .customer_name
+                                                        }}
+                                                    </div>
                                                 </div>
                                             </div>
 
@@ -145,47 +113,40 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                                                     </thead>
                                                     <tbody>
                                                         <tr
-                                                            v-for="item in itemList"
+                                                            v-for="item in props.items"
                                                             :key="item.id"
                                                         >
                                                             <td
                                                                 class="border-b-2 border-gray-200 px-4 py-3"
                                                             >
-                                                                {{ item.id }}
+                                                                {{
+                                                                    item.item_id
+                                                                }}
                                                             </td>
                                                             <td
                                                                 class="border-b-2 border-gray-200 px-4 py-3"
                                                             >
-                                                                {{ item.name }}
+                                                                {{
+                                                                    item.item_name
+                                                                }}
                                                             </td>
                                                             <td
                                                                 class="border-b-2 border-gray-200 px-4 py-3"
                                                             >
-                                                                {{ item.price }}
+                                                                {{
+                                                                    item.item_price
+                                                                }}
                                                             </td>
                                                             <td
                                                                 class="border-b-2 border-gray-200 px-4 py-3"
                                                             >
-                                                                <select
-                                                                    name="quantity"
-                                                                    v-model="
-                                                                        item.quantity
-                                                                    "
-                                                                >
-                                                                    <option
-                                                                        v-for="q in quantity"
-                                                                        :value="
-                                                                            q
-                                                                        "
-                                                                    >
-                                                                        {{ q }}
-                                                                    </option>
-                                                                </select>
+                                                                {{
+                                                                    item.quantity
+                                                                }}
                                                             </td>
                                                             <td>
                                                                 {{
-                                                                    item.price *
-                                                                    item.quantity
+                                                                    item.subtotal
                                                                 }}
                                                             </td>
                                                         </tr>
@@ -194,25 +155,73 @@ const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
                                             </div>
 
                                             <div class="p-2 w-full">
-                                                <div class="">
-                                                    <label for="" class=""
-                                                        >合計金額</label
-                                                    >
-                                                    <div
-                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    >
-                                                        {{ totalPrice }} 円
-                                                        <div></div>
-                                                    </div>
-
-                                                    <div class="p-2 w-full">
-                                                        <button
-                                                            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                                                        >
-                                                            登録
-                                                        </button>
-                                                    </div>
+                                                <label for="" class=""
+                                                    >合計金額</label
+                                                >
+                                                <div
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    {{
+                                                        props.order[0].total
+                                                            ? props.order[0]
+                                                                  .total
+                                                            : 0
+                                                    }}
+                                                    円
                                                 </div>
+                                            </div>
+
+                                            <div class="p-2 w-full">
+                                                <label for="" class=""
+                                                    >ステータス</label
+                                                >
+                                                <div
+                                                    v-if="
+                                                        props.order[0].status ==
+                                                        true
+                                                    "
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    未キャンセル
+                                                </div>
+
+                                                <div
+                                                    v-if="
+                                                        props.order[0].status ==
+                                                        false
+                                                    "
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    キャンセル済
+                                                </div>
+                                            </div>
+
+                                            <divdiv class="p-2 w-full">
+                                                <label for="" class=""
+                                                    >キャンセル日</label
+                                                >
+                                                <div
+                                                    v-if="
+                                                        props.order[0].status ==
+                                                        false
+                                                    "
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                >
+                                                    {{
+                                                        dayjs(
+                                                            props.order[0]
+                                                                .updated_at
+                                                        ).format("YYYY/MM/DD")
+                                                    }}
+                                                    円
+                                                </div>
+                                            </divdiv>
+                                            <div class="p-2 w-full">
+                                                <button
+                                                    class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                                                >
+                                                    登録
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
